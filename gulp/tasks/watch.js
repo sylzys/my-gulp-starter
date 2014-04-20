0d04bbf9d6260e95a67afc0034eb7c22856eff95
Syl Zys <sylzys@gmail.com>
2014-04-20 19:01:49 +0200

    running 'mocha' task when JS file in modified

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ var livereload = require('gulp-livereload');
 
 gulp.task('watch', function(){
 	//gulp.watch('src/js/**', ['browserify']);
-	gulp.watch('src/js/**', ['jshint']);
+	gulp.watch('src/js/**', ['jshint', 'mocha']);
 	gulp.watch('src/sass/**', ['sass']);
 	gulp.watch('src/img/**', ['imagemin']);
 	gulp.watch('test/test_*.js', ['mocha']);
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,3 @@
 document.getElementById("toto");
 console.log("msg");
+if (i == ""){};
